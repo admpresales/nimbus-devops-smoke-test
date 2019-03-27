95c95eb66dc5b639eaae3de5d311060bfa886459
Jason Hrabi <jason.hrabi@microfocus.com>
2019-03-27 12:17:32 -0400

    Initial commit

---
+++ src/main/java/DevOpsSmokeTest.java
@@ -0,0 +1,40 @@
+import java.io.*;
+
+public class DevOpsSmokeTest{
+    public static void main(String[] args) {
+        String s = null;
+        String dockerCommand[] = new String[4];
+        dockerCommand[0] = "nimbusapp devops:1.1.7.0 -s DEVOPS_TAG=latest up";
+        dockerCommand[1] = "nimbusapp leanft-chrome:14.52 up";
+        dockerCommand[2] = "nimbusapp octane:12.60.35.186 -s OCTANE_TAG=latest up";
+        dockerCommand[3] = "nimbusapp aos:1.1.7 up";
+
+        Process p;
+        BufferedReader stdInput;
+        BufferedReader stdError;
+
+        for(int i = 0; i< dockerCommand.length; i++) {
+            try {
+                System.out.println(dockerCommand[i]);
+                p = Runtime.getRuntime().exec(dockerCommand[i]);
+
+                stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+
+                // read the output from the command
+                while ((s = stdInput.readLine()) != null) {
+                    System.out.println(s);
+                }
+
+                // read any errors from the attempted command
+                while ((s = stdError.readLine()) != null) {
+                    System.out.println(s);
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(-1);
+            }
+        }
+    }
+}
